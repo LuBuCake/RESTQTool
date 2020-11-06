44db07a2d91626a87428341b570d676f9d2e7137
Lucas <madneshell@outlook.com>
2020-11-06 19:30:39 -0300

    Update Main.Designer.cs

--- RESTQTool/RESTQTool/Main.Designer.cs
+++ RESTQTool/RESTQTool/Main.Designer.cs
@@ -1,4 +1,21 @@
-﻿namespace RESTQTool
+/*
+This file is part of RESIDENT EVIL XSEW Tool.
+
+RESIDENT EVIL STQ Tool is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+RESIDENT EVIL STQ Tool is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied
+warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with RESIDENT EVIL STQ Tool. If not, see <https://www.gnu.org/licenses/>6.
+*/
+
+namespace RESTQTool
 {
     partial class Main
     {
